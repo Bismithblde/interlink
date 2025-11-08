5b7baf09a880671703bdc2707bb48be3f3be1a18
Bismithblde <ryan917rc@gmail.com>
2025-11-08 12:33:14 -0500

    init

--- client/app/(main)/FindFriend.tsx
+++ client/app/(main)/FindFriend.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-import FindFriendFilter from '@/src/components/ui/FindFriend/FindFriendFilter';
+import React from "react";
+import { View, Text } from "react-native";
+import FindFriendFilter from "@/src/components/ui/FindFriend/FindFriendFilter";
 
 export default function FindFriendPage() {
   return (
